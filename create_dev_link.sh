756356abafe411ea34d67a5e4dc60cda11c22b99
damoxc <damoxc@b3ab561c-7021-0410-825b-ae720ed029d9>
2008-10-05 16:04:11 +0000

    rename the graph plugin to the stats plugin
    git-svn-id: http://svn.deluge-torrent.org/trunk/deluge/plugins/stats@3927 b3ab561c-7021-0410-825b-ae720ed029d9

--- create_dev_link.sh
+++ create_dev_link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir temp
 export PYTHONPATH=./temp
-python setup.py build develop --install-dir ./temp
-cp ./temp/Graph.egg-link ~/.config/deluge/plugins
+python setup.py develop --install-dir ./temp
+cp ./temp/Stats.egg-link ~/.config/deluge/plugins
 rm -fr ./temp
